6c13d886a4e2bce1b472e54ba3c07844275cb1dd
Oskar Schamardin <oskarotto.schamardin@tptlive.ee>
2020-02-04 13:45:47 +0200

    init 2

---
+++ cursor_focus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace cursor_focus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach(var process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
+                }
+            }
+        }
+    }
+}
